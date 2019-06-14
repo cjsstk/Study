fd7b2d0f1358c63dcb8e1a0ff31af6c4eb2fb894
cjsstk <cjsstk805@gmail.com>
2019-06-15 02:38:35 +0900

    채널링 타겟 대상이 instigator나 자신은 되지 않도록 함.

--- Source/Study/StudyChannelingActor.cpp
+++ Source/Study/StudyChannelingActor.cpp
@@ -45,6 +45,11 @@ void AStudyChannelingActor::BeginPlay()
 
 void AStudyChannelingActor::OnBeginOverlapComponent(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
+	if (OtherActor == this || OtherActor == Instigator)
+	{
+		return;
+	}
+
 	TargetActors.AddUnique(OtherActor);
 }
 
